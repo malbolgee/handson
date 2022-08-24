3dcd5c77ef7ec9d0cdcc9da5575eaa9c2a1eb61d
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-08-24 18:52:04 -0400

    feat: Implement sniffer code for esp32
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

---
+++ sniffer_sep32/sniffer.ino
@@ -0,0 +1,120 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_wifi_types.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+
+#define LED_GPIO_PIN                     5
+#define WIFI_CHANNEL_SWITCH_INTERVAL  (500)
+#define WIFI_CHANNEL_MAX               (13)
+
+uint8_t level = 0, channel = 1;
+
+static wifi_country_t wifi_country = {.cc="CN", .schan = 1, .nchan = 13};
+
+typedef struct {
+  unsigned frame_ctrl:16;
+  unsigned duration_id:16;
+  uint8_t addr1[6];
+  uint8_t addr2[6];
+  uint8_t addr3[6];
+  unsigned sequence_ctrl:16;
+  uint8_t addr4[6];
+} wifi_ieee80211_mac_hdr_t;
+
+typedef struct {
+  wifi_ieee80211_mac_hdr_t hdr;
+  uint8_t payload[0];
+} wifi_ieee80211_packet_t;
+
+wifi_promiscuous_pkt_t *ppkt;
+
+static esp_err_t event_handler(void *ctx, system_event_t *event);
+static void wifi_sniffer_init(void);
+static void wifi_sniffer_set_channel(uint8_t channel);
+static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+  return ESP_OK;
+}
+
+void wifi_sniffer_init(void)
+{
+  nvs_flash_init();
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_country(&wifi_country));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
+  ESP_ERROR_CHECK(esp_wifi_start());
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
+}
+
+void wifi_sniffer_set_channel(uint8_t channel)
+{
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+}
+
+/** This is the call back function called by the esp_wifi_set_promiscuous_rx_cb. 
+    This function is called even of the user is not interacting with the Serial.
+    
+    Once the user give an input, the while will trap the code until the command
+    is finished being processed. 
+*/
+void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
+{
+  if (type != WIFI_PKT_MGMT)
+    return;
+
+    // In the original code, this was a local const variable,
+    // in order to access it inside the getRssi function, it was made
+    // into a global variable;
+    ppkt = (wifi_promiscuous_pkt_t *)buff;
+    
+    String serialCommand;
+    while (Serial.available() > 0) {
+        char serialChar = Serial.read();
+        serialCommand += serialChar; 
+
+        if (serialChar == '\n') {
+            processCommand(serialCommand);
+            serialCommand = "";
+        }
+    }
+}
+
+void setup() {
+  Serial.begin(115200);
+  wifi_sniffer_init();
+}
+
+void loop() {
+  vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
+  wifi_sniffer_set_channel(channel);
+  channel = (channel % WIFI_CHANNEL_MAX) + 1;
+}
+
+/** This function process the comand giver by the user through the Serial. */
+void processCommand(String command)
+{
+  command.trim();
+  command.toUpperCase();
+
+  // TODO: for now, we're just returning the RSSI, but in the future,
+  // we'll need more information, like the MAC Address.
+  if (command == "GET_RSSI")
+      Serial.printf("RES GET_RSSI %d\n", getRssi());
+}
+
+/** This function returns the RSSI from the captured packet */
+int getRssi()
+{
+    return ppkt->rx_ctrl.rssi;
+}
