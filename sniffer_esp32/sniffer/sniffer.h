69b93d201615d9697568e019b4861d0872df8816
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-08-31 19:35:55 -0400

    faet: Extract SSID information from packet
    This commit implements the code that extracts the SSID from the captured
    packet.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

---
+++ sniffer_esp32/sniffer/sniffer.h
@@ -0,0 +1,69 @@
+#ifndef __SNIFFER_H__
+#define __SNIFFER_H__
+
+typedef enum
+{
+    ASSOCIATION_REQ,
+    ASSOCIATION_RES,
+    REASSOCIATION_REQ,
+    REASSOCIATION_RES,
+    PROBE_REQ,
+    PROBE_RES,
+    NU1,  /* ......................*/
+    NU2,  /* 0110, 0111 not used */
+    BEACON,
+    ATIM,
+    DISASSOCIATION,
+    AUTHENTICATION,
+    DEAUTHENTICATION,
+    ACTION,
+    ACTION_NACK,
+} wifi_mgmt_subtypes_t;
+
+typedef struct {
+  unsigned interval:16;
+  unsigned capability:16;
+  unsigned tag_number:8;
+  unsigned tag_length:8;
+  char ssid[0];
+  uint8_t rates[1];
+} wifi_mgmt_beacon_t;
+
+typedef struct {
+    unsigned protocol:2;
+    unsigned type:2;
+    unsigned subtype:4;
+    unsigned to_ds:1;
+    unsigned from_ds:1;
+    unsigned more_frag:1;
+    unsigned retry:1;
+    unsigned pwr_mgmt:1;
+    unsigned more_data:1;
+    unsigned wep:1;
+    unsigned strict:1;
+} wifi_header_frame_control_t;
+
+typedef struct {
+  wifi_header_frame_control_t frame_ctrl;
+//  unsigned duration_id:16;
+  uint8_t addr1[6];
+  uint8_t addr2[6];
+  uint8_t addr3[6];
+  unsigned sequence_ctrl:16;
+  uint8_t addr4[6];
+} wifi_ieee80211_mac_hdr_t;
+
+typedef struct {
+  wifi_ieee80211_mac_hdr_t hdr;
+  uint8_t payload[0];
+} wifi_ieee80211_packet_t;
+
+#define WIFI_CHANNEL_SWITCH_INTERVAL  (500)
+#define WIFI_CHANNEL_MAX               (13)
+
+esp_err_t event_handler(void *ctx, system_event_t *event);
+void wifi_sniffer_init(void);
+void wifi_sniffer_set_channel(uint8_t channel);
+void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
+
+#endif
