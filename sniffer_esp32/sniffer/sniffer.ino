e5c77619c76f098416be71ed77f87fe9bf048bab
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-09-10 21:48:39 -0400

    fix: Fix #4 board sometimes not responding
    RCA: Sometimes the captured packet at the moment of the board request is
    not of type WIFI_PKT_MGMT and subtype BEACON. For these cases there was
    no response whatsoever, causing the kernel module to wait until a
    timeout occurs.
    
    SOL: Now, the board writes to the Serial a default string naming the
    network as UNKNOWN (SSID) and giving a 0 as its RSSI value.
    
    This commit also brings better documentation and changes the names
    of some structs, as well as improviments regarding the code identation.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- sniffer_esp32/sniffer/sniffer.ino
+++ sniffer_esp32/sniffer/sniffer.ino
@@ -8,99 +8,124 @@
 
 uint8_t level = 0, channel = 1;
 
-static wifi_country_t wifi_country = {.cc="CN", .schan = 1, .nchan = 13};
+static wifi_country_t wifi_country = {.cc = "CN", .schan = 1, .nchan = 13};
 
-wifi_promiscuous_pkt_t *ppkt;
-wifi_ieee80211_packet_t *ipkt;
-wifi_ieee80211_mac_hdr_t *hdr;
-wifi_header_frame_control_t *frame_ctrl;
+wifi_promiscuous_pkt_t *promiscuous_packet;
+packet_t *packet;
+mac_hdr_t *hdr;
+hdr_frame_control_t *frame_ctrl;
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
-  return ESP_OK;
+    return ESP_OK;
 }
 
 void wifi_sniffer_init(void)
 {
-  nvs_flash_init();
-  tcpip_adapter_init();
-  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-  ESP_ERROR_CHECK(esp_wifi_set_country(&wifi_country));
-  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
-  ESP_ERROR_CHECK(esp_wifi_start());
-  esp_wifi_set_promiscuous(true);
-  esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
+    nvs_flash_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_country(&wifi_country));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_set_promiscuous(true);
+    esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
 }
 
 void wifi_sniffer_set_channel(uint8_t channel)
 {
-  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+    esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 }
 
-/** This is the call back function called by the esp_wifi_set_promiscuous_rx_cb. 
-    This function is called even of the user is not interacting with the Serial.
-    
-    Once the user give an input, the while will trap the code until the command
-    is finished being processed. 
-*/
-void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
+/**
+ * This is the callback function called by the esp_wifi_set_promiscuous_rx_cb.
+ * This function is called even of the user is not interacting with the Serial.
+ *
+ * Once the user give an input, the while will trap the code until the command
+ * is finished being processed.
+ */
+void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type)
 {
-  if (type != WIFI_PKT_MGMT)
-    return;
+    // if (type != WIFI_PKT_MGMT)
+    //     return;
 
-    ppkt = (wifi_promiscuous_pkt_t *)buff;
-    ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
-    hdr = (wifi_ieee80211_mac_hdr_t *)&ipkt->hdr;
+    // ppkt = (wifi_promiscuous_pkt_t *)buff;
+    // ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
+    // hdr = (wifi_ieee80211_mac_hdr_t *)&ipkt->hdr;
 
-    frame_ctrl = (wifi_header_frame_control_t *)&hdr->frame_ctrl;
+    // frame_ctrl = (wifi_header_frame_control_t *)&hdr->frame_ctrl;
 
     String serialCommand;
-    while (Serial.available() > 0) {
+    while (Serial.available() > 0)
+    {
         char serialChar = Serial.read();
         serialCommand += serialChar;
 
-        if (serialChar == '\n') {
-            processCommand(serialCommand);
+        if (serialChar == '\n')
+        {
+            processCommand(serialCommand, buff);
             serialCommand = "";
         }
     }
 }
 
-void setup() {
-  Serial.begin(9600);
-  wifi_sniffer_init();
+void setup()
+{
+    Serial.begin(9600);
+    wifi_sniffer_init();
 }
 
 void loop()
 {
-  wifi_sniffer_set_channel(channel);
-  channel = (channel % WIFI_CHANNEL_MAX) + 1;
+    wifi_sniffer_set_channel(channel);
+    channel = (channel % WIFI_CHANNEL_MAX) + 1; // change channel every 500 miliseconds
 
-  delay(500);
+    delay(500);
 }
 
 /** This function process the comand given by the user through the Serial. */
-void processCommand(String command)
+void processCommand(String command, void *buff)
 {
-  command.trim();
-  command.toUpperCase();
+    command.trim();
+    command.toUpperCase();
 
-  if (command == "GET_NETWORK_INFO")
-        get_network_info();
+    if (command == "GET_NETWORK_INFO")
+        get_network_info(buff);
 }
 
-/** This function returns the RSSI and SSID from the captured packet */
-void get_network_info()
+/**
+ * This function parses a packet and writes a string to the Serial interface
+ * containing the SSID and RSSI of the captured packet.
+ *
+ * Test if the packet captured is of subtype #BEACON and of type #WIFI_PKT_MGMT.
+ * If it is, try to filter out the ones that have no SSID.
+ *
+ * Returns nothing.
+ *
+ * Writes to the Serial a string containing the SSID and RSSI of
+ * the captured packet.
+ *
+ * If the packet does not have the type and subtype required, write to the serial
+ * a default string containing a network named UNKNOWN and 0 as the RSSI value.
+ */
+void get_network_info(void *buff)
 {
 
-    // TODO: what if the packet is not a beacon?
+    promiscuous_packet = (wifi_promiscuous_pkt_t *)buff;
+    packet = (packet_t *)promiscuous_packet->payload;
+    hdr = (mac_hdr_t *)&packet->hdr;
+
+    frame_ctrl = (hdr_frame_control_t *)&hdr->frame_ctrl;
+
     if (frame_ctrl->type == WIFI_PKT_MGMT && frame_ctrl->subtype == BEACON)
     {
-        const wifi_mgmt_beacon_t *beacon = (wifi_mgmt_beacon_t *) ipkt->payload;
+        const mgmt_beacon_t *beacon = (mgmt_beacon_t *)packet->payload;
         if (beacon->tag_length > 0)
-           Serial.printf("RES GET_NETWORK_INFO %.*s %d\n", beacon->tag_length, beacon->ssid, ppkt->rx_ctrl.rssi);
+            Serial.printf("RES GET_NETWORK_INFO %.*s %d\n", beacon->tag_length, beacon->ssid, promiscuous_packet->rx_ctrl.rssi);
     }
+    else
+        Serial.printf("RES GET_NETWORK_INFO UNKNOWN 0\n");
 }
